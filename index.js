a29dfb856e511e82cc2cb97e3239130620af8141
Pierrick GRANDCLEMENT-CHAFFY <pierrick.grandclement@gmail.com>
2021-07-13 00:32:22 +0200

    Suggestions channel
    Adding a suggestion system that automatically add check and reject reactions to a suggestion message

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const Discord = require('discord.js')
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
 require('dotenv').config()
+const globalConfig = require('./src/config/global.json')
+
+const Suggestion = require('./src/events/suggestion')
 
 client.on('ready', function () {
     client.user.setActivity("Code with ❤️", {
         type: "WATCHING"
     }).catch(console.error)
     console.log('Bot started !')
+})
+
+client.on('message', function (message) {
+    if (message.author.bot) return
+    if (message.channel.id == globalConfig.suggestions_channel_id) {
+        Suggestion.parse(message)
+    }
 })
 
 client.login(process.env.DISCORD_TOKEN)
---
+++ src/events/suggestion.js
@@ -0,0 +1,38 @@
+const globalConfig = require('../config/global.json')
+const Event = require('./event')
+
+module.exports = class Suggestion extends Event {
+
+    static action(message) {
+        if (message.content.length > 10) {
+            const suggestionEmbed = {
+                color: globalConfig.embed_color,
+                title: 'Suggestion de ' + message.author.username,
+                description: message.content,
+                timestamp: new Date(),
+                footer: {
+                    text: 'Proposé par ' + message.author.username,
+                    icon_url: 'https://cdn.discordapp.com/avatars/' + message.author.id + '/' + message.author.avatar + '.png',
+                },
+            }
+            message.channel.send({
+                embed: suggestionEmbed
+            }).then(function (sendMessage) {
+                sendMessage.react('✅')
+                sendMessage.react('❌')
+            })
+        } else {
+            const errorEmbed = {
+                color: globalConfig.embed_color,
+                title: 'Merci de nous donner plus de détails sur votre suggestion (plus de 10 caractères)',
+            }
+            message.reply({
+                embed: errorEmbed
+            }).then(response => {
+                response.delete({ timeout: 5000 })
+            })
+        }
+        message.delete()
+    }
+
+}
